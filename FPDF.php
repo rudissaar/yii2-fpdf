4a913e65888b49e5e1f0f2513a45ce088d557a2e
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2017-09-12 13:49:10 +0100

    Development.

--- FPDF.php
+++ FPDF.php
@@ -1,13 +1,10 @@
 <?php
 
-namespace kevinmurda;
+namespace kevinmurda\FPDF;
 
 use \FPDF as BaseFPDF;
 
 class FPDF extends BaseFPDF
 {
-    public function __construct()
-    {
-        parent::construct();
-    }
+    
 }
