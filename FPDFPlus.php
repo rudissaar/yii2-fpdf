b62e32cfcb07ddb6ed940ad93f9c95e97abd782e
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2017-12-06 19:00:21 +0000

    Added GetPageHeight and GetPageWidth methods to FPDFPlus class.

--- FPDFPlus.php
+++ FPDFPlus.php
@@ -21,11 +21,37 @@ class FPDFPlus extends FPDF
     /**
      *
      * @param float|null $height
-     * @param string $hmtl
+     * @param string $html
      * @param string $link
      */
     public function WriteHtml($height, $html, $link = '')
     {
         $this->WriteLn($height, strip_tags($html), $link);
+    }
+
+    /**
+     *
+     * @return float
+     */
+    public function GetPageHeight()
+    {
+        $height = $this->h;
+        $topMargin = $this->tMargin;
+        $bottomMargin = $this->bMargin;
+
+        return (float) ($height - $topMargin - $bottomMargin);
+    }
+
+    /**
+     *
+     * @return float
+     */
+    public function GetPageWidth()
+    {
+        $width = $this->w;
+        $leftMargin = $this->lMargin;
+        $rightMargin = $this->rMargin;
+
+        return (float) ($width - $rightMargin - $leftMargin);
     }
 }
